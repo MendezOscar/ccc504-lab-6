d3184c2e604b70405f09813eec6eef75e6928ca5
user <user@users-MacBook-Pro.local>
2018-10-26 09:47:02 -0600

    lab-6

---
+++ models/company.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+//get the Schema class
+const Schema = mongoose.Schema;
+
+const CompanySchema = new Schema({
+    name: {
+        required: true,
+        type: String
+    },
+    city: String,
+    address: String
+});

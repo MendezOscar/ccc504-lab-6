b22398d30575113f1f05790634568744ffc0f347
user <user@users-MacBook-Pro.local>
2018-10-26 22:30:42 -0600

    token ath

--- server.js
+++ server.js
@@ -16,7 +16,71 @@ mongoose.connection.on('error', err => {
     console.log('error while connecting to mongodb', err);
 });
 
-server.connection({host: '127.0.0.1', port: '3000'});
+/*
+const user = {
+    1: {
+        id: 'hapi_db.lala',
+        name: 'lala'
+    }
+};
+
+const validate = async function (decoded, request) {
+
+    if (!user[decoded.id]) {
+        return { isValid: false };
+    }
+    else {
+        return { isValid: true };
+    }
+};
+
+const init = async () => {
+
+    
+    await server.register(require('hapi-auth-jwt2'));
+
+    server.auth.strategy('jwt', 'jwt',
+        {
+            key: 'NeverShareYourSecret',
+            validate: validate,
+            verifyOptions: { algorithms: ['HS256'] }
+        });
+
+    server.auth.default('jwt');
+
+    server.connection({ host: '127.0.0.1', port: '3000' });
+
+    server.route([
+        {
+          method: "GET", path: "/", config: { auth: false },
+          handler: function(request, reply) {
+            reply({text: 'Token not required'});
+          }
+        },
+        {
+          method: 'GET', path: '/restricted', config: { auth: 'jwt' },
+          handler: function(request, reply) {
+            reply({text: 'You used a Token!'})
+            .header("Authorization", request.headers.authorization);
+          }
+        },
+      ]);
+
+      await server.start();
+      return server;
+
+    server.route(companyRoutes);
+};
+
+init().then(server => {
+    console.log('Server running at:', server.info.uri);
+  })
+  .catch(error => {
+    console.log(error);
+  });*/
+// ======================= No Funciona =======================
+
+server.connection({ host: '127.0.0.1', port: '3000' });
 server.route({
     path: '/',
     method: 'GET',
